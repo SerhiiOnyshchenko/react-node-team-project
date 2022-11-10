6a44fd45741cd33b572aba3d21b3bc8964a697ae
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-11-10 13:38:28 +0200

    start files

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import s from './index.module.css';
 
-export default function Button({ text, active = false }) {
-  return <div className={active ? s.Btn_active : s.Btn}>{text}</div>;
+export default function Button({ text, active = false, smole = false }) {
+  return (
+    <div
+      className={active ? s.Btn_active : s.Btn}
+      style={smole ? { height: '32px', fontSize: '14px', width: 'auto' } : {}}
+    >
+      {text}
+    </div>
+  );
 }
