6698c245af8d52a21f76a3c3f75cbba137601711
Zoryana <zfedorovska@gmail.com>
2022-11-13 10:33:08 +0200

    added background picture for register page

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AuthForm from 'components/AuthForm';
 import Container from 'components/Container';
+import s from './index.module.css';
 
 export default function RegisterPage() {
   return (
-    <Container>
-      <AuthForm />
-    </Container>
+    <div className={s.waveBackground}>
+      <Container>
+        <AuthForm />
+      </Container>
+    </div>
   );
 }
