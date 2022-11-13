b670d6c7ba1e32197fe33a230b6b0dec32b98918
Natalia Monasturska <tjomamon@gmail.com>
2022-11-13 22:16:40 +0200

    start work

--- src/pages/NewsPage/index.jsx
+++ src/pages/NewsPage/index.jsx
@@ -1,6 +1,67 @@
-import React from 'react';
-// import s from './index.module.css';
 
-export default function NewsPage() {
-  return <div>NewsPage </div>;
+import React, { useState } from 'react';
+import s from './index.module.css';
+import { News } from 'components/ContactList/Contact/Contact';
+
+export default function NewsPage(){
+  const [news, setNews] = useState([]);
+
+
+
+  const handleSubmitInputName = event => {
+    event.preventDefault();
+    // const nameNormalized = name.toLowerCase();
+    // const dobbleName = items.find(
+    //   item => item.name.toLowerCase() === nameNormalized
+    // );
+    // if (dobbleName) {
+    //   Notiflix.Notify.failure(`${name} is already in contacts`);
+    //   return
+    // }
+    // dispatch(CreateContact(
+    //   {
+    //     name,
+    //     number,
+    //   }));
+    // setName('')
+    // setNumber('')
+  };
+  return (
+    <div className={s.newsContainer}>
+         <h1 className={s.newsTitle}>News</h1>
+         <form className={s.form} onSubmit={handleSubmitInputName}>
+         <input
+            className={s.input}
+            // onChange={handleChangeInput}
+            // value={number}
+            type="tel"
+            name="number"
+           placeholder='Search'
+            required
+          />
+<button type="submit" className={s.btn}></button>
+         </form>
+         <ul className={s.newList}>
+        {news.map(item => (
+          <News
+            key={item.id}
+            news={item}
+          />))
+}
+    </ul>
+         {/* /* <ContactForm />
+        <h2 className={css.title}>Contacts</h2>
+        <Filter />
+        {status === 'fetch' && items.length === 0 && (<div className={css.Loader}>
+            <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="150"
+                visible={true}
+            />
+        </div>)}
+        // {userStatus !== 'refreshUser' && <ContactList />  */}
+    </div>
+)
 }
