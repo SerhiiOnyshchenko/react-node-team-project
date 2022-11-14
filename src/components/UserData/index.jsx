545bfcfc87f7f5703ca17bed41c0734d14eb6589
Ihor Sharkadi <igor.sharkadi.93@gmail.com>
2022-11-14 20:46:31 +0200

    feat: basics for user card

--- src/components/UserData/index.jsx
+++ src/components/UserData/index.jsx
@@ -1,15 +1,32 @@
-// import s from './index.module.css';
+import s from './index.module.css';
 import UserDataItem from 'components/UserDataItem';
+
+// temporary user
+const user = {
+  img: './avatar.jpg',
+  name: 'Arthur Fleck',
+  email: 'joker@mail.com',
+  birthday: '21.11.1946',
+  phone: '+380976666666',
+  city: 'Gotham',
+};
 
 export default function UserData() {
   return (
     <div>
-      UserData
-      <UserDataItem></UserDataItem>
-      <UserDataItem></UserDataItem>
-      <UserDataItem></UserDataItem>
-      <UserDataItem></UserDataItem>
-      <UserDataItem></UserDataItem>
+      <div className={s.imgWrap}>
+        <img
+          className={s.image}
+          src={require(`${user.img}`)}
+          alt={`${user.name}`}
+        />
+      </div>
+      <button type="button">Edit photo</button>
+      <UserDataItem field="Name" value={user.name}></UserDataItem>
+      <UserDataItem field={'Email'} value={user.email}></UserDataItem>
+      <UserDataItem field={'Birthday'} value={user.birthday}></UserDataItem>
+      <UserDataItem field={'Phone'} value={user.phone}></UserDataItem>
+      <UserDataItem field={'City'} value={user.city}></UserDataItem>
     </div>
   );
 }
--- src/components/UserDataItem/index.jsx
+++ src/components/UserDataItem/index.jsx
@@ -1,5 +1,11 @@
 // import s from './index.module.css';
 
-export default function UserDataItem() {
-  return <div>UserDataItem</div>;
+export default function UserDataItem({ field, value }) {
+  return (
+    <div>
+      <p>{field}</p>
+      <p>{value}</p>
+      <button type="button">Edit</button>
+    </div>
+  );
 }
