19789aebf5fbab2c1469fa2cf8277a33aac79c1f
Natalia2715 <92229205+Natalia2715@users.noreply.github.com>
2022-11-14 18:35:49 +0200

    loginForm styled

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -3,19 +3,13 @@ import React from 'react';
 import s from './index.module.css';
 import LoginForm from '../../components/LoginForm';
 import Container from 'components/Container';
-import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
   return (
-    <Container>
-      <h1 className={s.title}>Login</h1>
-      <LoginForm />
-      <p className={s.question}>
-        Don't have an account?{' '}
-        <Link to="/register" className={s.registerLink}>
-          Register
-        </Link>
-      </p>
-    </Container>
+    <div className={s.background}>
+      <Container>
+        <LoginForm />
+      </Container>
+    </div>
   );
 }
