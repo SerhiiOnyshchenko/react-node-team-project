1c86de381d68e20dbaa0a5feda891613b55a192e
Natalia Monasturska <tjomamon@gmail.com>
2022-11-15 01:26:49 +0200

    visual part of the desktop application

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,10 +1,24 @@
-import logo from '../../images/logo.png'
 import s from './index.module.css';
 
-export default function Logo() {
+
+export default function News({ news }) {
+  const dataNormalize = news.date.replace(/(\d{4}).*(\d{2}).*(\d{2})/, '$3/$2/$1')
+
   return (
-    <div className={s.logoWrapper}>
-      <img src={logo} alt="logo" aria-label='logo' />
+    
+    <div className={s.container}>
+      <div className={s.decor}></div>
+      <div className={s.containerTitle}>
+        <div className={s.title}>
+          {news.title}
+        </div>
+       <p className={s.text}>{news.description}</p>
+       <div className={s.containerDataAndReadMore}>
+        <span className={s.data}>{dataNormalize}</span>
+        <a className={s.linkNews}  target="_blank" rel="noreferrer"  href={news.url}>Read more</a>
+       </div>
+      </div>
+
     </div>
   )
 }
