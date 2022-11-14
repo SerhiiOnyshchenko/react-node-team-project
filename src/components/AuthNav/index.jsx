7005fd4ff4ad54bb7f83c71e670173d684947822
KizievDmitriy <kizievdmitrii@gmail.com>
2022-11-14 18:05:34 +0200

    add condition rendering

--- src/components/AuthNav/index.jsx
+++ src/components/AuthNav/index.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import Button from '../Button';
 import s from './index.module.css';
 
@@ -8,12 +9,18 @@ const itemsNav = [
 ];
 
 export default function AuthNav() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <ul className={s.list}>
       {itemsNav.map(({ href, text }) => (
         <li className={s.item} key={href}>
           <NavLink to={href}>
-            <Button text={text} />
+            {isMobile ? (
+              <Button text={text} smole={true} />
+            ) : (
+              <Button text={text} smole={false} />
+            )}
           </NavLink>
         </li>
       ))}
