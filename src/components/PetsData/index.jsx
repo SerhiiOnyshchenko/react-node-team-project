3bb5e00bb26c5885c5a04500bc4da4797cf4dc09
Andrii Krupa <krpaandrii@gmail.com>
2022-11-16 01:44:44 +0200

    In PetsData component Changed ModalAddsPet to ModalPage in index.jsx, removed excessive styles from index.module.css

--- src/components/PetsData/index.jsx
+++ src/components/PetsData/index.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-// import ModalAddsPet from 'components/ModalAddsPet';
 // import PetsList from 'components/PetsList';
 import s from './index.module.css';
 import Container from 'components/Container';
+// import ModalPage from 'pages/ModalPage';
 
 export default function PetsData(props) {
   const [showModal, setShowModal] = useState(false);
@@ -19,14 +19,13 @@ export default function PetsData(props) {
             <h2 className={s.title}>My pets: </h2>
             <div className={s.addPetWrapper}>
               <h3 className={s.addPetTitle}>Add pet</h3>
-              <button onClick={onAddPetBtnClick} className={s.buttonAddPet}>
-                {/* <svg fill="white" width="24" height="24">
-                  <use href="../../images/svg/plus.svg" />
-                </svg> */}
-              </button>
+              <button
+                onClick={onAddPetBtnClick}
+                className={s.buttonAddPet}
+              ></button>
             </div>
           </div>
-          {/* {showModal && <ModalAddsPet setShowModal={setShowModal} />} */}
+          {/* {showModal && <ModalPage setShowModal={setShowModal} />} */}
           {/* <PetsList /> */}
         </div>
       </Container>
