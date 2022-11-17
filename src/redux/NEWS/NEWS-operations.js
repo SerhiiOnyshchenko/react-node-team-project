cb5f9133bc7536b6cdab2320d5a0de289df15f79
Natalia Monasturska <tjomamon@gmail.com>
2022-11-17 14:16:56 +0200

    operations

--- src/redux/NEWS/NEWS-operations.js
+++ src/redux/NEWS/NEWS-operations.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://{url}';
+axios.defaults.baseURL = 'http://localhost:8083/api';
 
-export const newsOperations = createAsyncThunk('news', async () => {
+export const newsOperations = createAsyncThunk('news/getNews', async () => {
   try {
-    const { data } = await axios.get('/');
+    const { data } = await axios.get('/news');
     return data;
   } catch (error) {
     console.log('fetch-new-error', error);
