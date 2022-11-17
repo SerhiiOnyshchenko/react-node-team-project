60602eaa8774a78ac6632e585bb6bf6e62321550
Natalia Monasturska <tjomamon@gmail.com>
2022-11-18 01:19:40 +0200

    create axios  get and fix err

--- src/redux/NEWS/NEWS-operations.js
+++ src/redux/NEWS/NEWS-operations.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:8083/api';
+axios.defaults.baseURL = 'http://localhost:8000/api';
+let result;
 
 export const getNews = createAsyncThunk('news/getNews', async ({query}) => {
+console.log('query 8 строка',query);
   try {
-    const { data } = await axios.get(`/news?query=${query}`);
-    console.log(data);
-    return data;
+    if(query === undefined){
+     result = await axios.get('/news?');
+     console.log("result",result);
+     console.log('query === undefined');
+    }
+    if(query){
+      result = await axios.get(`/news?query=${query}`);
+      console.log('query есть');
+    }
+  
+   
+    console.log('data in getNews',result.data.data);
+    return result.data.data;
   } catch (error) {
     console.log('fetch-news-error', error);
   }
