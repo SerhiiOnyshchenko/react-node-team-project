67f095c262fbe82cf47e8eeacc7b745349c85eac
IlliaBekarevych <93090373+IlliaBekarevych@users.noreply.github.com>
2022-11-18 13:21:45 +0200

    Update notices-operation

--- src/redux/notices/notices-operation.js
+++ src/redux/notices/notices-operation.js
@@ -3,9 +3,9 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'team-project-backend.onrender.com/api';
 
-const noticesCategories = createAsyncThunk(`/notices/category`, async () => {
+const noticesCategories = createAsyncThunk('/notices', async ({ category }) => {
   try {
-    const { data } = await axios.get(`/notices/category`);
+    const { data } = await axios.get(`/notices/${category}`);
     return data;
   } catch (error) {
     console.log(error);
