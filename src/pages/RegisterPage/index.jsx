cfc39aa8129acbea8faa2007311451136202fd1b
Serhii Onyshchenko <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-11-18 07:30:22 +0200

    add rerender navigate

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AuthForm from 'components/AuthForm';
 import Container from 'components/Container';
 import s from './index.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
+  let isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/user');
+    }
+  }, [navigate, isLoggedIn]);
   return (
     <div className={s.waveBackground}>
       <Container>
