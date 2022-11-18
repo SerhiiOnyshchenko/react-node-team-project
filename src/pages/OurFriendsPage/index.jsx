ada23f35364e3a91dc28abb1a664d6a1fd9e4847
Kashyrets Ivanna <kasirecivanna@gmail.com>
2022-11-18 12:39:51 +0200

    friends

--- src/pages/OurFriendsPage/index.jsx
+++ src/pages/OurFriendsPage/index.jsx
@@ -1,55 +1,45 @@
 import React from 'react';
 import styles from './index.module.css';
-import ItemFrien from './ItemFriend';
+import ItemFrien from '../../components/ItemFriend';
+import { useGetFriendQuery } from 'redux/friends/rtkFriends';
+import Loader from 'components/Loader';
 
 export default function OurFriendsPage() {
+  const { isLoading, data, error, isUninitialized } = useGetFriendQuery();
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.align_container}>
-          <ItemFrien
-            header={'Cellogs'}
-            time={'20:00'}
-            adress={'location'}
-            email={'kashyretsivanna@gmail.com'}
-            phone={'0969875235'}
-            avatar={
-              'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fmdshakibdhali9%2Fnew-photo-style%2F&psig=AOvVaw3ksSmVLeuFALQw640uoM-y&ust=1668294823547000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCK2tSgp_sCFQAAAAAdAAAAABAE'
-            }
-          />
-          <ItemFrien
-            header={'Cellogs'}
-            time={'20:00'}
-            adress={'location'}
-            email={'kashyretsivanna@gmail.com'}
-            phone={'0969875235'}
-            avatar={
-              'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fmdshakibdhali9%2Fnew-photo-style%2F&psig=AOvVaw3ksSmVLeuFALQw640uoM-y&ust=1668294823547000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCK2tSgp_sCFQAAAAAdAAAAABAE'
-            }
-          />
-          <ItemFrien
-            header={'Cellogs'}
-            time={'20:00'}
-            adress={'location'}
-            email={'kashyretsivanna@gmail.com'}
-            phone={'0969875235'}
-            avatar={
-              'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fmdshakibdhali9%2Fnew-photo-style%2F&psig=AOvVaw3ksSmVLeuFALQw640uoM-y&ust=1668294823547000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCK2tSgp_sCFQAAAAAdAAAAABAE'
-            }
-          />
-          <ItemFrien
-            header={'Cellogs'}
-            time={'20:00'}
-            adress={'location'}
-            email={'kashyretsivanna@gmail.com'}
-            phone={'0969875235'}
-            avatar={
-              'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fmdshakibdhali9%2Fnew-photo-style%2F&psig=AOvVaw3ksSmVLeuFALQw640uoM-y&ust=1668294823547000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCK2tSgp_sCFQAAAAAdAAAAABAE'
-            }
-          />
-          ;
-        </div>
-      </div>
-    </div>
+    <>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isUninitialized ? (
+        <div>{isUninitialized} - Currently skipped</div>
+      ) : isLoading ? (
+        <Loader />
+      ) : data ? (
+        <>
+          <div className={styles.wrapper}>
+            <div className={styles.container}>
+              <div className={styles.align_container}>
+                {console.log(data)}
+                {data[0] &&
+                  data.map(el => (
+                    <ItemFrien
+                      key={el._id}
+                      site={el.addressUrl}
+                      header={el.title}
+                      time={el.workDays ? el.workDays : []}
+                      adress={el.address ? el.address : '------------------'}
+                      email={el.email ? el.email : '------------------'}
+                      phone={el.phone ? el.phone : '------------------'}
+                      avatar={el.imageUrl ? el.imageUrl : '------------------'}
+                    />
+                  ))}
+                ;
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import loaderReduser from './loader/loader-reduser';
+import { friendsApi } from './friends/rtkFriends';
 
 const NEW_PersistConfig = {
   key: 'NEW',
@@ -31,14 +32,19 @@ export const store = configureStore({
     auth: persistReducer(authPersistConfig, authReducer),
     NEW: persistReducer(NEW_PersistConfig, NEW_Reduser),
     loader: loaderReduser,
+    [friendsApi.reducerPath]: friendsApi.reducer,
   },
   middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    ...getDefaultMiddleware(
+      {
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
       },
-    }),
+      friendsApi.middleware
+    ).concat(friendsApi.middleware),
   ],
+
   devTools: process.env.NODE_ENV === 'development',
 });
 
