c09d23bf94f5877ce29071dc61eb6df2e123cdc0
KizievDmitriy <kizievdmitrii@gmail.com>
2022-11-19 20:26:59 +0200

    add notification if not loginIn user

--- src/components/AddNoticeButton/index.jsx
+++ src/components/AddNoticeButton/index.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
+
 export default function AddNoticeButton() {
-  return <div>AddNoticeButton</div>;
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/auth/auth-selectors';
+import s from './index.module.css';
+import { ReactComponent as Plus } from '../../images/svg/plus.svg';
+import { useMediaQuery } from 'react-responsive';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function AddNoticeButton() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const text = 'Add pet';
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  const handleModalToggle = () => {
+    if (!isLoggedIn) return toast.info('You must be logged in');
+  };
+  return (
+    <div className={s.btnWrapper}>
+      {!isMobile && text}
+      <button type="button" className={s.btn} onClick={handleModalToggle}>
+        <Plus className={s.icon} />
+        {isMobile && text}
+      </button>
+      <ToastContainer />
+    </div>
+  );
 }
