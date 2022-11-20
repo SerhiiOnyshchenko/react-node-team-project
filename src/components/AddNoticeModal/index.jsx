82990ea9e86f73a35f99b9b13d1807b9befd1f08
KizievDmitriy <kizievdmitrii@gmail.com>
2022-11-20 13:00:55 +0200

    add noticemobile

---
+++ src/components/AddNoticeModal/index.jsx
@@ -0,0 +1,15 @@
+import Container from 'components/Container';
+import ModalPage from 'pages/ModalPage';
+import React from 'react';
+
+export default function AddNoticeModal() {
+  return (
+    <>
+      <ModalPage>
+        <Container>
+          <h2>Add pet</h2>
+        </Container>
+      </ModalPage>
+    </>
+  );
+}
