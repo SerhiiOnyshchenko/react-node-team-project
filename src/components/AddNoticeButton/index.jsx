d42c1f9a23cabe0f67c6391a5d88eac3608cf2e6
KizievDmitriy <kizievdmitrii@gmail.com>
2022-11-20 13:00:33 +0200

    add styles

--- src/components/AddNoticeButton/index.jsx
+++ src/components/AddNoticeButton/index.jsx
@@ -1,28 +1,37 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import authSelectors from '../../redux/auth/auth-selectors';
 import s from './index.module.css';
 import { ReactComponent as Plus } from '../../images/svg/plus.svg';
 import { useMediaQuery } from 'react-responsive';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 
 export default function AddNoticeButton() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const text = 'Add pet';
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const [isOpen, setOpen] = useState(false);
 
-  const handleModalToggle = () => {
+  const handleClick = () => {
     if (!isLoggedIn) return toast.info('You must be logged in');
+    else {
+      setOpen(true);
+    }
   };
   return (
-    <div className={s.btnWrapper}>
-      {!isMobile && text}
-      <button type="button" className={s.btn} onClick={handleModalToggle}>
-        <Plus className={s.icon} />
-        {isMobile && text}
-      </button>
-      <ToastContainer />
-    </div>
+    <>
+      <div className={s.btnWrapper}>
+        {!isMobile && text}
+        <button
+          type="button"
+          className={s.btn}
+          onClick={handleClick}
+          isOpen={isOpen}
+        >
+          <Plus className={s.icon} />
+          {isMobile && text}
+        </button>
+      </div>
+    </>
   );
 }
