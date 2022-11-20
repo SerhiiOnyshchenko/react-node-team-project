0a54d6361bc44498e6339a9591c17e51d44497ad
Alex Borets <alexborets2812@gmail.com>
2022-11-20 19:14:00 +0200

    styles finalized, NoticesPage refactoring, NoticesSearch finalized

--- src/pages/NoticesPage/index.jsx
+++ src/pages/NoticesPage/index.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-// import NoticesSearch from 'components/NoticesSearch';
-// import NoticesCategoriesNav from 'components/NoticesCategoriesNav';
-// import NoticesCategoriesList from 'components/NoticesCategoriesList';
-// import AddNoticeButton from 'components/AddNoticeButton';
+import NoticesSearch from 'components/NoticesSearch';
+import NoticesCategoriesNav from 'components/NoticesCategoriesNav';
+import NoticesCategoriesList from 'components/NoticesCategoriesList';
+import AddNoticeButton from 'components/AddNoticeButton';
 import s from './index.module.css';
+import { useState } from 'react';
+// import { useSelector } from 'react-redux';
 
 export default function NoticesPage() {
+  const notices = [];
+  // useSelector(noticesSelectors.getNoticesCategories());
+  const [filteredNotices, setFilteredNotices] = useState(notices);
+
+  const filter = search => {
+    const newNotices = notices.filter(notice =>
+      notice.titleOfAd.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setFilteredNotices(newNotices);
+  };
+
   return (
-    <div className={s.container}>
-      {/* <NoticesSearch></NoticesSearch>
+    <div className={s.Container}>
+      <NoticesSearch onSearch={filter}></NoticesSearch>
       <NoticesCategoriesNav></NoticesCategoriesNav>
-      <NoticesCategoriesList></NoticesCategoriesList>
-      <AddNoticeButton></AddNoticeButton> */}
+      <NoticesCategoriesList list={filteredNotices}></NoticesCategoriesList>
+      <AddNoticeButton></AddNoticeButton>
     </div>
   );
 }
