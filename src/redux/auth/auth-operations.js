39cd9894edac326fba1fd8337b05bf16acc85ec2
Сергій Онищенко <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-11-23 11:30:49 +0300

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -60,7 +60,7 @@ const fetchCurrentUser = createAsyncThunk(
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
-    
+
     if (persistedToken === null) {
       return thunkAPI.rejectWithValue();
     }
