1b1015a0c426ff0219f589a590f22e41d5c78e9f
Serhii Onyshchenko <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-11-29 10:55:08 +0200

    deploy to netlify

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="react-node-team-project">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
